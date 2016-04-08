685097c361f000c57707255e46c137c03b8b1da3
Laine Janne TTK14S <jannelaine@kamk.fi>
2016-04-09 00:20:27 +0300

    GameManager updated

---
+++ WorkWork/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("level1");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
