fed0f8535e753f9c90660828fd41153c6acfb138
Laine Janne TTK14S <jannelaine@kamk.fi>
2016-04-08 22:23:21 +0300

    Merging stuff

---
+++ WorkWork/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public GameObject player;
+    public Transform target;
+
+    public bool lookAtPlayer;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("Player");
+        target = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (lookAtPlayer)
+            transform.LookAt(target);
+	}
+}
--- WorkWork/Assets/Scripts/GameManager.cs
+++ WorkWork/Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    public CharacterController pc;
 
+    private int finalScore;
 
     bool startTimer;
     public float timer;
@@ -13,30 +18,77 @@ public class GameManager : MonoBehaviour {
 
     public int origFontSize;
 
+    private bool endGame;
+
+    public List<GameObject> collectables;
+    public int collectablesAmount;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        Player = GameObject.Find("Player");
+        pc = Player.GetComponent<CharacterController>();
+
         canvasText = GameObject.Find("ScoreText");
         scoreText = canvasText.GetComponent<Text>();
 
+        endGame = false;
         startTimer = false;
         timer = 10.0f;
         origFontSize = 40;
+        collectablesAmount = 10;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        TimerBehaviour();
+	void Update ()
+    {
+        TimerBehavior();
 
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SpawnCollectables();
+            startTimer = true;
+        }
+
+        if (endGame)
+        {
+            finalScore = Player.GetComponent<CharacterController>().GetScore();
+            print("FINAL SCORE IS " + finalScore);
+            //do stuff on endgame
+        }
 
 	}
 
-    void TimerBehaviour()
+    void SpawnCollectables()
     {
-        if (timer > 0)
-            timer -= 1.0f * Time.deltaTime;
-        if (timer <= 0)
-            timer = 0;
+        for (int i = 0; i < collectablesAmount; i++)
+        {
+            GameObject collectable = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            collectable.AddComponent<SphereCollider>();
+            float tempX = Random.Range(-15, 15);
+            float tempZ = Random.Range(-15, 15);
+            collectable.transform.position = new Vector3(tempX, 0, tempZ);
+            collectables.Add(collectable);
+        }
+    }
 
+    void TimerBehavior()
+    {
+        if (startTimer)
+        {
+            if (timer > 0)
+                timer -= 1.0f * Time.deltaTime;
+            if (timer <= 0)
+                timer = 0;
+        }
+       
 
         if (timer < 5.05f && timer > 4.95f)
             scoreText.fontSize = 60;
@@ -45,5 +97,8 @@ public class GameManager : MonoBehaviour {
 
         if (scoreText != null)
             scoreText.text = timer.ToString("F2");
+
+        if (timer <= 0)
+            endGame = true;
     }
 }
