68b4fe50b7b045bfcbc4533b7b476ce691ee1a60
Laine Janne TTK14S <jannelaine@kamk.fi>
2016-04-08 20:50:39 +0300

    Player Controller done

---
+++ WorkWork/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+    public float jumpStrength = 10.0f;
+    public float interactSpeed = 0.2f;
+
+
+    private Quaternion targetRotation;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Controls();
+
+
+	}
+
+    void Controls()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, +Input.GetAxis("Vertical"));
+        Vector3 moveAngle = new Vector3(0, +0, 45);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpStrength);
+           
+        }
+    }
+}
