e57be28fe1c6c599b61d92083e6207de363da67d
Laine Janne TTK14S <jannelaine@kamk.fi>
2016-04-10 16:42:24 +0300

    a lot of stuff

---
+++ WorkWork/Assets/Scripts/BlockController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+    int health;
+    bool isDead;
+
+	// Use this for initialization
+	void Start () {
+
+        isDead = false;
+        health = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int half = (int)(health * 0.5f);
+        if (health <= half)
+        {
+
+        }
+        if (health <= 0)
+        {
+            isDead = true;
+        }
+	}
+
+    public void GotHit()
+    {
+        health -= 10;
+    }
+
+    public bool GetIsDead()
+    {
+        return isDead;
+    }
+}
